b4dba43c28e653f110a08d0c69b204204cdecc62
Amit Dimri <dimriamit98@gmail.com>
2022-04-02 01:06:29 +0530

    refactored auth service to factory function

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,29 +1,24 @@
 export default function AuthService(apiClient) {
-	this.apiClient = apiClient;
+  this.apiClient = apiClient;
 
-	this.dummy= function() {
-		return this.apiClient.post("auth/dummy");
-	}
+  this.forgotPasswordAPI = function (email) {
+    return this.apiClient.post("auth/forgot-password", { email });
+  };
 
-	this.forgotPasswordAPI = function(email) {
-		return this.apiClient.post("auth/forgot-password", { email });
-	}
+  this.sendEmailVerification = function (isRegistration) {
+    return this.apiClient.post(
+      `auth/send-email-verification?isRegistration=${isRegistration}`
+    );
+  };
 
-	this.sendEmailVerification = function(isRegistration) {
-		return this.apiClient.post(
-			`auth/send-email-verification?isRegistration=${isRegistration}`
-		);
-	}
+  this.verifyEmail = function (token) {
+    return this.apiClient.post(`auth/verify-email?token=${token}`);
+  };
 
-	this.verifyEmail = function(token) {
-		return this.apiClient.post(`auth/verify-email?token=${token}`);
-	}
-
-	this.refreshTokens = function() {
-		return this.apiClient.post("/auth/refresh-tokens");
-	}
-	this.logout = function() {
-		return this.apiClient.post("/auth/logout");
-	}
-
+  this.refreshTokens = function () {
+    return this.apiClient.post("/auth/refresh-tokens");
+  };
+  this.logout = function () {
+    return this.apiClient.post("/auth/logout");
+  };
 }
